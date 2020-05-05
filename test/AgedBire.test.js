257b11ef502219e9b6e7eaeaae6e48f44a3d646a
Xiaofeng Su <xiaofeng.su@thoughtworks.com>
2020-05-05 20:47:26 +0800

    fix typo

--- test/AgedBire.test.js
+++ test/AgedBire.test.js
@@ -1,12 +1,12 @@
-import AgedBire from '../main/AgedBrie';
+import AgedBrie from '../main/AgedBrie';
 
 describe('AgedBrie', () => {
   it('should quality increase when time goes by when days more than sellIn', () => {
-    const agedBire = new AgedBire(3, 12);
-    expect(agedBire.getPrice(5)).toEqual(17);
+    const agedBrie = new AgedBrie(3, 12);
+    expect(agedBrie.getPrice(5)).toEqual(17);
   });
   it('should quality increase when time goes by when days less than sellIn', () => {
-    const agedBire = new AgedBire(3, 12);
-    expect(agedBire.getPrice(2)).toEqual(14);
+    const agedBrie = new AgedBrie(3, 12);
+    expect(agedBrie.getPrice(2)).toEqual(14);
   });
 });
